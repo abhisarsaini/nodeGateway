ad720df66591fc863c0367c9cfbd55188837fec8
Suman Bishayee <suman.bishayee@snapdeal.com>
2015-04-24 14:17:34 +0530

    committing changes for proxy implementation.

--- nodeAPIGateway/node-server.js
+++ nodeAPIGateway/node-server.js
@@ -3,36 +3,22 @@ var url = require("url");
 var loopback = require('loopback');
 var app = module.exports = loopback();
 var boot = require('loopback-boot');
+
+//Proxy setup
 var proxy = require('./middleware/proxy');
 var proxyOptions = require('./middleware/proxy/config.json');
-var app = module.exports = loopback();
+
+//Rate Limiting 
 var rateLimiting = require('./middleware/rate-limiting');
 
+//Starting proxy server.
 app.listen(3000);
+console.log('API Gateway Server running at http://127.0.0.1:3000');
 
 // Key Verification
 
 app.use(rateLimiting({type :'quota', limit: 10, interval: 20000}));
 app.use(rateLimiting({type :'spike-arrest', limit: 3, interval: 3000}));
 
+//routing via proxy server.
 app.use(proxy(proxyOptions));
-
-console.log('Server running at http://127.0.0.1:3000/');
-
-http.createServer(function(req, res) {
-	console.log('Inside index node.');
-	setTimeout(function() {
-	    res.end("inside index node.");
-	}, 3);
-	
-	}).listen(3001);
-
-
-http.createServer(function(req, res) {
-	console.log('-----------------------------Inside api node.');
-	res.end("inside api node.");
-	//res.end("Request received on 8082");
-	
-	
-	}).listen(3002);
-
